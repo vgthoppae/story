d7a6c8905dd94fe9981c69c16719718821e5e8b6
Venkataganesh Thoppae <tkvganesh@gmail.com>
2021-08-26 20:49:57 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBa7H-oCE4gJVFixewXpyBUlPKge5gkwz8",
+  authDomain: "create-story-dc1b5.firebaseapp.com",
+  projectId: "create-story-dc1b5",
+  storageBucket: "create-story-dc1b5.appspot.com",
+  messagingSenderId: "196511689238",
+  appId: "1:196511689238:web:7f556ae950ec5e0000e337"
+};
